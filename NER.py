74f42fa0c13379de0444ceec30eccc307700ce78
Ilsommo97 <76537627+Ilsommo97@users.noreply.github.com>
2022-11-09 09:11:51 +0100

    some progress

--- NER.py
+++ NER.py
@@ -1,21 +1,30 @@
+from venv import create
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 from transformers import pipeline
-from reading import extract_id, createDictionary
+from reading import extract_id, createDictionary, createCorpus
 
+
+#Handling the texts
 PATH_FAIRY_TALES =   './FAIRY_TALE/texts/'
 PATH_SHORT_STORIES = './SHORT_STORY/texts/'
-
-
-
 FtDictionary = createDictionary(PATH_FAIRY_TALES)
 SsDictionary = createDictionary(PATH_SHORT_STORIES)
 
 
+corpus = createCorpus(PATH_SHORT_STORIES, PATH_FAIRY_TALES)
 
+print(corpus)
+
+
+
+
+#Creating the bert model
 tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
 model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
-
 nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+
+
+
 """
 Limitations and bias
 This model is limited by its training dataset of entity-annotated news articles from a specific span of time. 
@@ -25,9 +34,30 @@ Furthermore, the model occassionally tags subword tokens as entities and post-pr
 results may be necessary to handle those cases.
 !!!
 """
-for text in SsDictionary:
+""" for text in SsDictionary:
     result = nlp(SsDictionary[text])
     print(SsDictionary[text])
     print(result)
     break
+ """
 
+
+def TSV(path: str, dictionary_short: dict, dictionary_tales: dict):
+    """
+    This function will output the tsv file with the predicted result in the required TSV format.
+    For the time being, we have several problems. The biggest one is probably the one regarding
+    subwords getting tagged. We need to post process the results and try to approach the NER tagging
+    from different directions, i.e finding wikipedia references, using different models and maybe even create
+    a gold dataset for this specific problem and train an existing NER tagger on the created dataset.
+    """
+
+    with open(path + 'result.tsv', 'w') as w:
+        for i in range(10):
+            w.write('hey' + '\t ciao' + '\t just another one\n')
+         
+
+""" 
+    
+TSV('./', SsDictionary, FtDictionary)
+
+ """
--- reading.py
+++ reading.py
@@ -27,8 +27,36 @@ def createDictionary(path: str):
       #text holds the id and the name of the story (we actually dont need the name of the story)
       #r.read() reads and returns the text of the story
       dictionary[extract_id(text)] = r.read()
+
       
   return dictionary
+
+
+def createCorpus(pathToShort: str, pathToTales: str):
+  """
+  This function is gonna create a unique document containing the texts in the fairy tale and short stories directories
+  Applying the BERT NER tagger directly to the output of this function is going to directly give us the required starting and 
+  ending offset charachter of each entity in the texts
+  """
+
+  file_list_tales = os.listdir(pathToTales) 
+  file_list_stories = os.listdir(pathToShort)
+  unique_document = ''
+  for text in file_list_tales:
+    with open(pathToTales + text, 'r') as r:
+      unique_document = unique_document + r.read()
+
+  for text in file_list_stories:
+    with open(pathToShort + text, 'r') as r:
+      unique_document = unique_document + '\n' +  r.read()
+
+  return unique_document
+
+
+
+
+
+
 
 
 
